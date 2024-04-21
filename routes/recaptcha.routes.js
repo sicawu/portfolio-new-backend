f4070f976fd68c3869850842e1a0b50c9100c2c8
Simona Wurst <simona_wu@yahoo.de>
2024-04-21 12:32:43 +0200

    Integrate contactform with reCAPTCHA to ContactPage

---
+++ routes/recaptcha.routes.js
@@ -0,0 +1,55 @@
+const router = require("express").Router()
+const axios = require('axios')
+const nodemailer = require('nodemailer')
+
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.USER_ME,
+        pass: process.env.PASS_ME
+    }
+})
+
+
+router.post('/submit-form', async (req, res) => {
+    const { recaptcha, name, mail, message } = req.body;
+
+    try {
+        const response = await axios.post('https://www.google.com/recaptcha/api/siteverify', null, {
+            params: {
+                secret: process.env.RECAPTCHA_SECRET_KEY,
+                response: recaptcha
+            }
+        })
+
+        const { success } = response.data
+
+        if (success) {
+            const mailOptions = {
+                from: process.env.USER_ME,
+                to: process.env.USER_ME,
+                subject: 'New Loveletter from my website',
+                text: `Name: ${name}, Email: ${mail}, Message: ${message}`
+            }
+
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    console.error('Error sending email:', error)
+                    res.status(500).send('Internal server error.')
+                } else {
+                    console.log('Email sent:', info.response)
+                    res.status(200).send('Form submitted successfully.')
+                }
+            })
+        } else {
+            res.status(400).send('reCAPTCHA verification failed.')
+        }
+    } catch (error) {
+        console.error('Error verifying reCAPTCHA:', error)
+        res.status(500).send('Internal server error.')
+    }
+})
+
+
+module.exports = router
